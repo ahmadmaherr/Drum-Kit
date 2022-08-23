2e989927c4eb3d0fa1d9642702e3d390c5710000
AHMED MAHER <ahmed.maher11@msa.edu.eg>
2022-08-23 02:19:48 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+  document.addEventListener("keypress",function(event){
+    makesound(event.key);
+
+    animation(event.key);
+
+  });
+
+  for(var i=0; i < 7; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+      var drumName1 = this.innerHTML;
+
+      makesound(drumName1);
+
+      animation(drumName1);
+    });
+    }
+
+    function makesound (drumName){
+     switch (drumName) {
+       case "w":
+       var audio = new Audio('sounds/tom-1.mp3');
+       audio.play();
+
+         break;
+       case "a":
+       var audio = new Audio('sounds/tom-2.mp3');
+     audio.play();
+     break;
+     case "s":
+     var audio = new Audio('sounds/tom-3.mp3');
+     audio.play();
+     break;
+     case "d":
+     var audio = new Audio('sounds/tom-4.mp3');
+     audio.play();
+     break;
+     case "j":
+     var audio = new Audio('sounds/snare.mp3');
+     audio.play();
+     break;
+     case "k":
+     var audio = new Audio('sounds/crash.mp3');
+     audio.play();
+     break;
+     case "l":
+     var audio = new Audio('sounds/kick-bass.mp3');
+     audio.play();
+     break;
+
+       default:
+
+     }
+   }
+
+function animation(keypressed)
+{
+  var b = document.querySelector("." + keypressed);
+  b.classList.add('pressed');
+  setTimeout(function(){b.classList.remove('pressed');}, 100);
+}
